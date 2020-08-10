74cfd180bd855b9e229bf0a311b14bfdb1ab4402
Gabriel Rufino <contato@gabrielrufino.com>
2020-08-10 20:37:47 -0300

    Option overwrite

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const { name, version } = require('./package.json')
 program
   .name(name)
   .version(version)
+  .option('-o, --overwrite', 'Sobrescreve o arquivo da imagem processada')
 
 program.parse(process.argv)
+
+if (program.overwrite) {
+  console.log('A imagem de origem será sobrescrita!')
+}
 
