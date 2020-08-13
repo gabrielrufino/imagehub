3439c743d8a43f7056f497c722ec6fcffd5360ae
Gabriel Rufino <contato@gabrielrufino.com>
2020-08-13 19:16:30 -0300

    Usando flag overwrite

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const { program } = require('commander')
 const sharp = require('sharp')
+const fs = require('fs')
 
 const { name, version } = require('./package.json')
 
@@ -14,17 +15,23 @@ program
   .command('resize <file>')
   .description('Redimensiona uma imagem')
   .action(file => {
-    sharp(file)
-      .resize({ width: 50 })
-      .toFile(`imagehub-${file}`)
-      .then(() => {
-        console.log(`Imagem redimensionada!`)
-      })
+    const resized = sharp(file).resize({ width: 50 })
+
+    if (program.overwrite) {
+      resized
+        .toBuffer()
+        .then(buffer => {
+          fs.writeFile(file, buffer, () => {
+            console.log('Imagem redimensionada')
+          })
+        })
+    } else {
+      resized
+        .toFile(`imagehub-${file}`)
+        .then(() => {
+          console.log(`Imagem redimensionada!`)
+        })
+    }
   })
 
 program.parse(process.argv)
-
-if (program.overwrite) {
-  console.log('A imagem de origem será sobrescrita!')
-}
-
